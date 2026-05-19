86e8004379dc057641b1c2141950e1aa036c3ed9
Ferran López Sierra <267028755+ferranlopezEngDev@users.noreply.github.com>
2026-05-19 12:17:30 +0200

    Add connection and node factories

--- hn3ttk/connections/__init__.py
+++ hn3ttk/connections/__init__.py
@@ -2,6 +2,12 @@ from hn3ttk.connections.base import Connection
 from hn3ttk.connections.custom_factor_polynomial import (
     CustomFactorPolynomialConnection,
 )
+from hn3ttk.connections.factory import (
+    available_connection_types,
+    connection_from_dict,
+    connection_to_dict,
+    register_connection_type,
+)
 from hn3ttk.connections.linear_interpolation import LinearInterpolationConnection
 from hn3ttk.connections.pipe_darcy import PipeDarcy
 from hn3ttk.connections.pipe_fixed_power_law import PipeFixedPowerLaw
@@ -18,4 +24,8 @@ __all__ = [
     "PipeLocalPowerLaw",
     "PolynomialRegressionConnection",
     "SplineInterpolationConnection",
+    "available_connection_types",
+    "connection_from_dict",
+    "connection_to_dict",
+    "register_connection_type",
 ]
